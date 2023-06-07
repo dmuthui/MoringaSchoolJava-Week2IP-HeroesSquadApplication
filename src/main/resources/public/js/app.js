738885132d6858faabde6e297d3594be97afd8b3
dmuthui <vyda2002us@gmail.com>
2023-06-08 00:38:48 +0300

    Adds the changes on squadForm and home.hbs plus the app.js

--- src/main/resources/public/js/app.js
+++ src/main/resources/public/js/app.js
@@ -1,26 +1,24 @@
-    //VALIDATES HEROFORM DATA ENTRY:SENDS ALERT WHEN THERE IS DUPLICATE HERO AND AVOIDS SENDING BLANK FORM
+//VALIDATES HEROFORM DATA ENTRY:SENDS ALERT WHEN THERE IS DUPLICATE HERO AND AVOIDS AVOIDS CLICKING BLANK FORM FOR SUBMISSION
     function validateForm() {
-        var heroName = document.getElementById("heroName").value;
-        var age = document.getElementById("age").value;
-        var specialPower = document.getElementById("specialPower").value;
-        var weakness = document.getElementById("weakness").value;
+      var heroName = document.getElementById("heroName").value.toUpperCase; // Trim whitespace
+      var age = document.getElementById("age").value.trim();
+      var specialPower = document.getElementById("specialPower").value;
+      var weakness = document.getElementById("weakness").value;
 
-        if (heroName === "" || age === "" || specialPower === "" || weakness === "") {
-            alert("Please fill in all the fields before submitting the form.");
-            return false; // Prevent form submission
-        }
-
-        // Check if hero name already exists
-        var existingHeroes = heroName;
-
-        if (existingHeroes.includes(heroName)) {
-            alert("Hero name already exists. Please choose a different name.");
-            document.getElementById("heroName").value = ""; // Clear the hero name input field
-            document.getElementById("age").value = ""; // Clear the hero age input field
-            document.getElementById("specialPower").value = ""; // Clear the specialPower input field
-            document.getElementById("weakness").value = ""; // Clear the weakness input field
-            return false; // Prevent form submission
-        }
-
-        return true; // Allow form submission
+      if (heroName === "" || age === "" || specialPower === "" || weakness === "") {
+        alert("Please fill in all the fields before submitting the form.");
+        return false; // Prevent form submission
+      }
     }
+
+
+//VALIDATES SQUADFORM DATA ENTRY:SENDS ALERT WHEN THERE IS DUPLICATE SQUAD AND AVOIDS CLICKING BLANK FORM FOR SUBMISSION
+ function validatesForm() {
+ var squad = document.getElementById("squad").value;
+ var size = document.getElementById("size").value;
+  var cause = document.getElementById("cause").value;
+     if (squad === "" || size === "" || cause === "") {
+     alert("Please fill in all the fields before submitting the form.");
+     return false; // Prevent form submission
+      }
+  }
